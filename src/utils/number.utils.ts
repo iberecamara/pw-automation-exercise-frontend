a67e722a7eefa7933ac1a2c22601252a531cf928
Iberê Camara <ibere.camara@gmail.com>
2026-07-01 12:54:45 -0300

    Updated NumberUtils to use Faker

--- src/utils/number.utils.ts
+++ src/utils/number.utils.ts
@@ -1,14 +1,9 @@
+import { faker } from "@faker-js/faker";
+
 export class NumberUtils {
 
     public static getRandomNumber(options?: { min?: number; max?: number }): number {
-        const min = options?.min ?? 0;
-        const max = options?.max ?? 1;
-
-        if (min >= max) {
-            throw new Error("Min must be less than max");
-        }
-
-        return Math.floor(Math.random() * (max - min) + min);
+        return faker.number.int({ min: options?.min, max: options?.max });
     }
 
 }
